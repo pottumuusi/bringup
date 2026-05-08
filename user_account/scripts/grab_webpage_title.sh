7a708965de5332f6a3a03c613977bfbedeab311a
Aleksi Backman <bddubbb@protonmail.com>
2026-05-08 19:19:31 +0300

    Exit on error and expand variable to be a single string

--- user_account/scripts/grab_webpage_title.sh
+++ user_account/scripts/grab_webpage_title.sh
@@ -5,13 +5,15 @@
 # 	* https://metacpan.org/dist/XML-Twig
 # 	* https://packages.debian.org/trixie/xml-twig-tools
 
+set -e
+
 main() {
 	local -r webpage_url="${1}"
 	local -r webpage_file="target_webpage.html"
 
 	pushd /tmp
 
-	curl ${webpage_url} > ${webpage_file}
+	curl "${webpage_url}" > ${webpage_file}
 	echo ""
 	xml_grep --html --nowrap title ${webpage_file}
 	echo ""
