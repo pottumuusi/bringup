454860c9a07c436996135634985220c882e99206
Aleksi Backman <bddubbb@protonmail.com>
2026-05-08 14:02:04 +0300

    Add script for grabbing webpage title

---
+++ user_account/scripts/grab_webpage_title.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Dependencies:
+# * XML-Twig
+# 	* https://metacpan.org/dist/XML-Twig
+# 	* https://packages.debian.org/trixie/xml-twig-tools
+
+main() {
+	local -r webpage_url="${1}"
+	local -r webpage_file="target_webpage.html"
+
+	pushd /tmp
+
+	curl ${webpage_url} > ${webpage_file}
+	echo ""
+	xml_grep --html --nowrap title ${webpage_file}
+	echo ""
+	rm ${webpage_file}
+
+	popd
+}
+
+main "${@}"
